7c83c44a13a36b30e48821c20cfc3325ea6e9ae4
secminhr <secminhrian@gmail.com>
2026-06-28 22:08:49 +0800

    feat: Filename display

--- app/Main.hs
+++ app/Main.hs
@@ -6,23 +6,24 @@ import Lib
 import Viewport
 import Editor
 import Lens.Micro.Platform (makeLenses, (%=), (.=), use)
-import Brick.Widgets.Core (str, reportExtent)
+import Brick.Widgets.Core (str, reportExtent, (<=>), vLimit, fill, (<+>))
 import qualified Brick.Widgets.Core as B
 import Brick.Types (Widget(..), BrickEvent, EventM, BrickEvent(VtyEvent), getContext, availWidth, availHeight, ViewportType(Horizontal), Extent(Extent))
 import qualified Brick.Types as BT
 import Brick.Main (customMain, neverShowCursor, showFirstCursor, resizeOrQuit, App(App), lookupExtent, continueWithoutRedraw, halt)
-import Brick.AttrMap (forceAttrMap)
-import Graphics.Vty (outputIface, Modifier (MCtrl))
+import Brick.AttrMap (forceAttrMap, AttrMap)
+import Graphics.Vty (outputIface, Modifier (MCtrl), withBackColor)
 import qualified Graphics.Vty.CrossPlatform as V
 import Graphics.Vty.Config (defaultConfig)
-import Graphics.Vty.Attributes (defAttr)
+import Graphics.Vty.Attributes (defAttr, withStyle, reverseVideo)
 import Graphics.Vty.Input.Events (Event(..), Key(..))
 import Graphics.Vty.Output (displayBounds)
 import Control.Monad.IO.Class (liftIO)
 import Debug.Trace (traceM, trace, traceShowId, traceShowWith, traceShow)
-import Editor (newEditor)
-import Brick (Location(Location))
+import Editor (newEditor, editedString)
+import Brick (Location(Location), AttrName, attrName, attrMap, withAttr)
 import Editing (Editing)
+import Brick.Widgets.Border (border, hBorder, vBorder)
 
 data AppState = AppState
     { _editor :: Editor
@@ -39,6 +40,14 @@ data Name
     | MainEditorCursor
     deriving (Eq, Ord, Show)
 
+reverseAttr :: AttrName
+reverseAttr = attrName "reverseText"
+
+theMap :: AttrMap
+theMap = attrMap defAttr 
+    [ (reverseAttr, defAttr `withStyle` reverseVideo)
+    ]
+
 main :: IO ()
 main = do 
     filename <- getLine
@@ -47,9 +56,9 @@ main = do
     initialVty <- V.mkVty defaultConfig
     (termW, termH) <- displayBounds (outputIface initialVty)
 
-    let state = AppState (newEditor content) new (Size (fromIntegral termW) (fromIntegral termH)) (map Just $ lines content) (CursorPos 0 0) filename
+    let state = AppState (newEditor content) new (Size (fromIntegral termW) (fromIntegral termH - 3)) (map Just $ lines content) (CursorPos 0 0) filename
 
-    let app = App drawUI showFirstCursor handleEvent (return ()) (const $ forceAttrMap defAttr)
+    let app = App drawUI showFirstCursor handleEvent (return ()) (const $ theMap)
 
     final <- customMain initialVty (return initialVty) Nothing app state
     return ()
@@ -86,7 +95,10 @@ updateStates = do
         Just (Extent _ _ (w, h)) -> let _ = (Size (fromIntegral w) (fromIntegral h)) in return ()
 
 drawUI :: AppState -> [Widget Name]
-drawUI s = [ drawEditor s ]
+drawUI s = [ 
+    (border (str (" " ++ _filename s ++ " "))) <+> (hBorder <=> str " " <=> hBorder) <=>
+    drawEditor s 
+    ]
 
 drawEditor :: AppState -> Widget Name
 drawEditor (AppState _ vp _ text (CursorPos row col) _) = reportExtent MainEditor $ B.showCursor MainEditorCursor (Location (fromIntegral col, fromIntegral row)) $ Widget BT.Greedy BT.Greedy $ do 
